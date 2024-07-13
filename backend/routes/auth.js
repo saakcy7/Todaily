23c843e0fa9cc92965c6ad868135d017abd60fe1
Sakshi KC <kcsakshi92@gmail.com>
2024-07-13 20:05:52 +0545

    password is set to gibberish

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const User = require("../models/user");
-
+const bcrypt=require("bcryptjs");
 //SIGNIN
 router.post("/register",async(req,res)=>{
     try{
         const {email,username,password}=req.body;
-        const user = new User({email,username,password});
+        const hashpassword=bcrypt.hashSync(password);
+        const user = new User({email,username,password:hashpassword});
         await user.save().then(()=>res.status(200).json({user:user}));
     }catch(error)
     {
