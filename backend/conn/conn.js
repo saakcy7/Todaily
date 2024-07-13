5e6d0432ea704507bbd70b02bc4e26fb4ed2a1a9
Sakshi KC <kcsakshi92@gmail.com>
2024-07-13 19:24:26 +0545

    connection to mongodb

---
+++ backend/conn/conn.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const conn = async(req,res)=>{
+    try{
+        await mongoose.connect('mongodb+srv://kcsakshi92:uIgjgxKyQPlmHTlN@cluster0.5obsf1a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
+            console.log('Connection to the database is successful');
+       })
+    } catch(err){
+        res.status(400).json({
+            message:'Internal Server Error',
+    });
+}
+};
+conn();
