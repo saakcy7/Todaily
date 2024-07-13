29e9f7361cba005c9c6b8997cbe5405109829062
Sakshi KC <kcsakshi92@gmail.com>
2024-07-13 20:16:09 +0545

    signin part in authjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/user");
-const bcrypt=require("bcryptjs");
+const bcrypt = require("bcryptjs");
 //SIGNIN
 router.post("/register",async(req,res)=>{
     try{
@@ -8,6 +8,28 @@ router.post("/register",async(req,res)=>{
         const hashpassword=bcrypt.hashSync(password);
         const user = new User({email,username,password:hashpassword});
         await user.save().then(()=>res.status(200).json({user:user}));
+    }catch(error)
+    {
+        console.log(error);
+        res.status(400).json({message:"User already exists"});
+    }
+});
+
+//SIGNIN
+router.post("/signin",async(req,res)=>{
+    try{
+        const user= await User.findOne({email:req.body.email});
+        if(!user)
+        {
+            res.status(400).json({message:"Please singup first"});
+        }
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password,user.password);
+        if(!isPasswordCorrect)
+        {
+            res.status(400).json({message:"Password is incorrect"});
+        }
+        const {password,...others}=user._doc;
+        res.status(200).json({others});
     }catch(error)
     {
         console.log(error);
