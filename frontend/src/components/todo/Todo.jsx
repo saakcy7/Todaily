07fdc64b75f897c36eada561248377ee87c083b0
Sakshi KC <kcsakshi92@gmail.com>
2024-09-08 14:13:50 +0545

    body shown after click

--- frontend/src/components/todo/Todo.jsx
+++ frontend/src/components/todo/Todo.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./todo.css";
 const Todo = () => {
+    const show=()=>{
+        document.getElementById("textarea").style.display="block";
+    };
 return(
     <>
 <div className="todo">
     <div className="todo-main container d-flex justify-content-center align-items-center">
-        <div className="d-flex flex-column todo-inputs w-25">
-        <input type="text" placeholder="TITLE" className="my-2"/>
-        <textarea type="text" placeholder="BODY"/>
+        <div className="d-flex flex-column todo-inputs-div w-50">
+        <input type="text" placeholder="TITLE" className="my-2 p-2 todo-inputs" onClick={show}/>
+        <textarea id="textarea"type="text" placeholder="BODY" className="p-2 todo-inputs"/>
         </div>
     </div>
     </div>
---
+++ frontend/src/components/todo/TodoCards.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const TodoCards = () => {
+    return(
+        <div className="p-3 todo-card">
+            <div>
+                <h5>Heading</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nostrum cumque repudiandae eum rem at veniam unde nulla totam! Saepe harum nostrum odit molestias optio provident nihil animi, esse pariatur.</p>
+            </div>
+        </div>
+    )
+};
+export default TodoCards;
